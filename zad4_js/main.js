4822129948f0c1f4e8d5f5bd8d81d1304bc2cb7d
OlexanderPaduh-77277 <aleksandrpaduh7@gmail.com>
2026-04-13 00:58:08 +0200

    zad4_js

---
+++ zad4_js/main.js
@@ -0,0 +1,62 @@
+const btnchenge = document.querySelector(".chenge-color-btn")
+const cv = document.querySelector(".cv")
+const footer = document.querySelector(".footer")
+const rightPart = document.querySelector(".right_part-cv")
+const leftPart = document.querySelector(".left_part-cv")
+const headerPhoto = document.querySelector(".headerPic")
+
+const accordeons = document.querySelectorAll(".accordion")
+
+let isRed = false
+
+btnchenge.addEventListener("click", () => {
+    console.log("btn clicked");
+    
+    if(!isRed){
+        cv.classList.add("red-cv")
+        footer.classList.add("content_red")
+        rightPart.classList.add("content_red")
+        leftPart.classList.add("content_red")
+        btnchenge.classList.add("content_red")
+        headerPhoto.src = "picture/header_pic-red.png"    // red image
+        
+    }else{
+        cv.classList.remove("red-cv")
+        footer.classList.remove("content_red")
+        rightPart.classList.remove("content_red")
+        leftPart.classList.remove("content_red")
+        btnchenge.classList.remove("content_red")
+        headerPhoto.src = "picture/header_pic-grean.png"    // red image
+
+        // btnchenge.style.setProperty("--hover-color","red")
+    }
+    isRed = !isRed
+
+  
+});
+
+accordeons.forEach(function(item){
+    item.addEventListener('click', showContent)
+})
+
+function showContent(isActive){
+        console.log("acc clicked")
+
+        accordeons.forEach(function(other){
+            if(other !== this){
+                other.nextElementSibling.style.maxHeight = null
+                other.classList.remove("active")
+            }
+
+        }, this)
+        const content = this.nextElementSibling
+        this.classList.toggle("active")
+        
+        if(content.style.maxHeight){
+            content.style.maxHeight = null
+        }else{
+            content.style.maxHeight = content.scrollHeight + "px"
+        }
+        
+
+    }
